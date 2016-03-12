e44c5cc04e31a7f4b9cc49e56ea70df4c2fda964
dtrindade <chapolin@gmail.com>
2016-03-11 23:33:40 -0300

    Adding header allow headers

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.static(__dirname + "/statics"));
 // Add headers
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   
   next();
 });
